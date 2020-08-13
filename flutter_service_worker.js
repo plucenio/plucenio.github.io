c585b9501f117506c8f98109d5fb123a390f7ca6
Filipe Plucenio <plucenio@gmail.com>
2020-08-13 13:30:45 -0300

    Update to new carousel style.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "7bcf39434337b4487b1df34ede5ca5f7",
 "/": "7bcf39434337b4487b1df34ede5ca5f7",
-"main.dart.js": "b961a1c83366314ee0de44c4e024278d",
+"main.dart.js": "e1e953a9cbd9542a9e1bab13722a4ae3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
