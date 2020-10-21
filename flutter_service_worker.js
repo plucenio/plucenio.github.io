0bfed27e5f4dbb5202f35d7fda674ed60fe51df5
Filipe Plucenio <plucenio@gmail.com>
2020-10-21 15:18:05 -0300

    New version to test. Device info plus.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,17 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "50237bdeeddf6e2c20f987788707e8c6",
-"/": "50237bdeeddf6e2c20f987788707e8c6",
-"main.dart.js": "bd57ce53d432e47157caa4e9788f0910",
+  "version.json": "463a1dc8caf8e0a4cf4cbeff130ee285",
+"index.html": "90c664ce0b1c5a87272dfd379ec3560d",
+"/": "90c664ce0b1c5a87272dfd379ec3560d",
+"main.dart.js": "8aa1144ff3b86473e13410b590e3c780",
+"web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "9243db17554fe9caff555548b83e2307",
-"assets/AssetManifest.json": "5c568f910f6da728951753762de0120f",
-"assets/NOTICES": "2563451a570692804a0154c17b03983c",
+"assets/AssetManifest.json": "201be07717a3aa6e6e5b3f0c4cdaad0d",
+"assets/NOTICES": "eaf21d2687626bb5a264c14d450c6caf",
 "assets/FontManifest.json": "d0d3335e68f43bb56bb61a20e7ab99fb",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/flutter_icons/fonts/Octicons.ttf": "73b8cff012825060b308d2162f31dbb2",
@@ -33,18 +35,26 @@ const RESOURCES = {
 "assets/packages/flutter_google_maps/assets/images/marker_a.png": "9b687e681fcc41298dfab9c1304b66d0",
 "assets/packages/flutter_google_maps/assets/images/marker_b.png": "bb8df73622307b582f89602743f4f02f",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/assets/images/Antonia.png": "51e988ebf5bdde7265d9aa9ff3ad89ea",
 "assets/assets/images/Happy.png": "74ea04100ada0400a5d6213a50f62bdb",
 "assets/assets/images/SplashScreen.png": "93eb8ead4cffaf16abca64a59731f827",
 "assets/assets/images/Hospital.png": "51afd0fe0dbb62e5732cbbbf90f44d1b",
+"assets/assets/images/IconLevel1.png": "37d74dbd9fa6b029a448bef3608a5e8a",
 "assets/assets/images/Trophy.png": "50a6a1becf553fef8b3a2908df6dec5a",
+"assets/assets/images/Graca.png": "5f1725ab850fa686c8464ec4f1e55e33",
 "assets/assets/images/NewBackground.png": "503e2c6df0d0cc5bbca61b4b4749cb61",
 "assets/assets/images/Neutral.png": "de82b4fe70d6d2b937247ccf0187dffc",
 "assets/assets/images/Sad.png": "da8279fbffedf8360e01180952f8240a",
 "assets/assets/images/GoldenTrophy.png": "65cba4a60e276e74328455e93a8fee5b",
 "assets/assets/images/RafaelRounded.png": "8f237f6ab03a4886547afcc53dc87f1a",
 "assets/assets/images/LogoSafety4Me.png": "db921a78d44f427d8df8465c8b5e6a27",
+"assets/assets/images/Hector.png": "ff7b15f2f3fcecada4c8dcc7ff84c033",
+"assets/assets/images/Ana.png": "907ac0383ff8f183a8f5a408c7519cfc",
 "assets/assets/images/Fernando.png": "f0538597ad37173e547341cbc345ec6e",
+"assets/assets/images/Heart.png": "d2e877804a8dafbdf15fc7c237f37bae",
 "assets/assets/images/SplashScreenBackground.png": "479d94df8cf1df3f4d13f107801c624b",
+"assets/assets/images/Florence.png": "068841f6ca5803cf94f553cf49d1ecc9",
+"assets/assets/images/Logo.png": "ad6b9366674e72f8ba959673bf03f766",
 "assets/assets/images/LogoSafety4MeWhitePacificBlue.png": "ae2a07b9516a46dddb1fdab6631db46e",
 "assets/assets/images/NewBackground3.png": "804e093cb66146a2d2fa1abfd346d6b3",
 "assets/assets/images/NewBackground2.jpg": "6ee8618e8db19fc702cf18ba13f7c3c4",
@@ -55,7 +65,8 @@ const RESOURCES = {
 "assets/assets/fonts/Roboto-MediumItalic.ttf": "cf23e1bb619029496260760b72aebd30",
 "assets/assets/fonts/Roboto-ThinItalic.ttf": "35a9c89aff1396595ad345e378a32aca",
 "assets/assets/fonts/Roboto-LightItalic.ttf": "a3ce4440f2abf76f4a1b14b83920138c",
-"assets/assets/fonts/Roboto-Thin.ttf": "321de678e592d0b8f44f1a82d7ca4b62"
+"assets/assets/fonts/Roboto-Thin.ttf": "321de678e592d0b8f44f1a82d7ca4b62",
+"assets/assets/animations/GameExplain.json": "1d5ea95b73ae09856dd1c2b7e9c024be"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -69,6 +80,7 @@ const CORE = [
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
@@ -137,6 +149,9 @@ self.addEventListener("activate", function(event) {
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
   var origin = self.location.origin;
   var key = event.request.url.substring(origin.length + 1);
   // Redirect URLs to the index.html
@@ -146,9 +161,10 @@ self.addEventListener("fetch", (event) => {
   if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
     key = '/';
   }
-  // If the URL is not the RESOURCE list, skip the cache.
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
   if (!RESOURCES[key]) {
-    return event.respondWith(fetch(event.request));
+    return;
   }
   // If the URL is the index.html, perform an online-first request.
   if (key == '/') {
@@ -172,10 +188,12 @@ self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
   if (event.data === 'skipWaiting') {
-    return self.skipWaiting();
+    self.skipWaiting();
+    return;
   }
-  if (event.message === 'downloadOffline') {
+  if (event.data === 'downloadOffline') {
     downloadOffline();
+    return;
   }
 });
 
